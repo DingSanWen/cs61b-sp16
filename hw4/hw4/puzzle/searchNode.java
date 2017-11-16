4eed18300f1cb84537e58c2ffc84019d7637ec51
DingSanWen <dingsanwen@126.com>
2017-11-16 18:15:14 +0800

    add searchNode, but haven't test it

---
+++ hw4/hw4/puzzle/searchNode.java
@@ -0,0 +1,43 @@
+package hw4.puzzle;
+
+import java.util.Comparator;
+
+/**
+ * Created by 丁天庆 on 2017/11/16.
+ */
+public class searchNode implements Comparable<searchNode> {
+    private Board board;
+    private searchNode prev;
+    private final int moves;
+
+    public searchNode(Board board) {
+        this.board = board;
+        moves = 0;
+    }
+
+    public searchNode(Board board, searchNode prev) {
+        this.board = board;
+        this.prev = prev;
+        this.moves = prev.moves + 1;
+    }
+
+//    public int moves() {
+//        return moves;
+//    }
+
+    public int getHamPriority() {
+        return moves + board.hamming();
+    }
+
+    public int getManPriority() {
+        return moves + board.manhattan();
+    }
+
+    public Board getBoard () {
+        return this.board;
+    }
+
+    public int compareTo(searchNode o) {
+        return this.getHamPriority() - o.getHamPriority();
+    }
+}
